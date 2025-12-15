31a6ad1df6b1184d60edb3961afa8ff1a78f9a2a
Josan Johnata Paixão Pontes <josanjohnata@gmail.com>
2025-12-15 00:34:13 +0000

    adiciona scripts lint:check e lint:fix

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Repositório para o desenvolvimento do curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home () {
-    return <h1>Eide, eu amo vc. E se vc me ama, me dá uma risadinha.</h1>
-};
+function Home() {
+  return <h1>Eide, eu amo vc. E se vc me ama, me dá uma risadinha.</h1>;
+}
 
 export default Home;
